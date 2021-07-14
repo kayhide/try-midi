3b010e6ab38eb435205482b3517e992fd6b88e87
Hideaki Kawai <kayhide@gmail.com>
2021-07-14 23:35:47 +0900

    Install tailwindcss

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
+craco: yarn start
 qsynth: qsynth
--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react';
-import './App.css';
 
 function Outputs(props) {
   const items = props.options.map(o => <option value={o.id}>{o.name}</option>);
@@ -42,19 +41,25 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <div className="App-keyboard">
-        <button onClick={play(0x45)}>C</button>
-        <button onClick={play(0x47)}>D</button>
-        <button onClick={play(0x48)}>D+</button>
-        <button onClick={play(0x49)}>E</button>
-        <button onClick={play(0x4a)}>F</button>
-        <button onClick={play(0x4c)}>G</button>
-        <button onClick={play(0x4e)}>A</button>
+    <div className="w-full h-screen flex flex-col bg-gray-200">
+      <div className="flex-grow flex items-center justify-center space-x-2">
+        <div>
+          <button className="px-4 py-2 border border-gray-400 rounded" onClick={play(0x45)}>C</button>
+          <button className="px-4 py-2 border border-gray-400 rounded" onClick={play(0x47)}>D</button>
+          <button className="px-4 py-2 border border-gray-400 rounded" onClick={play(0x48)}>D+</button>
+          <button className="px-4 py-2 border border-gray-400 rounded" onClick={play(0x49)}>E</button>
+          <button className="px-4 py-2 border border-gray-400 rounded" onClick={play(0x4a)}>F</button>
+          <button className="px-4 py-2 border border-gray-400 rounded" onClick={play(0x4c)}>G</button>
+          <button className="px-4 py-2 border border-gray-400 rounded" onClick={play(0x4e)}>A</button>
+          <button className="px-4 py-2 border border-gray-400 rounded" onClick={play(0x50)}>B</button>
+          <button className="px-4 py-2 border border-gray-400 rounded" onClick={play(0x51)}>C</button>
+        </div>
       </div>
-      <div className="App-controls">
-        <Outputs options={outputOptions} value={outputId} />
-        <Channels value={channel} onSelect={(c) => setChannel(_ => Number.parseInt(c))} />
+      <div className="flex-none flex justify-center">
+        <div className="m-2 p-4 border border-gray-400 rounded shadow space-x-4">
+          <Outputs options={outputOptions} value={outputId} />
+          <Channels value={channel} onSelect={(c) => setChannel(_ => Number.parseInt(c))} />
+        </div>
       </div>
     </div>
   );
